726d92b23f09e41e22ad5a222ac1b3cbc27f74fa
Chandu154 <devarachandra4@gmail.com>
2023-09-09 10:29:10 +0530

    terraform

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,6 +5,17 @@ terraform {
       version = "5.15.0"
     }
   }
+
+# we are telling to the terraform dont store sate in local ,store in backend means 
+# remote in s3 bucket and lock with dynamodb
+backend "s3" {
+  bucket = "roboshop-remote-state1"
+  key = "foreach-demo"
+  region= "us-east-1"
+  dynamodb_table = "roboshop-locking"
+  
+}
+
 }
 
 provider "aws" {
